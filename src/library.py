341027095461f0f842f31ba4542e325623e660a1
Mmesek <13630781+Mmesek@users.noreply.github.com>
2025-11-08 08:09:47 +0100

    Add functions to access song library
    Signed-off-by: Mmesek <13630781+Mmesek@users.noreply.github.com>

---
+++ src/library.py
@@ -0,0 +1,34 @@
+import os
+import msgspec
+from song import Song
+from pathlib import Path
+
+
+class LibrarySong(msgspec.Struct):
+    folder: str
+    playlist: str
+    song: Song
+
+
+def iterate_songs(path: str):
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            if file.endswith(".txt"):
+                yield os.path.join(root, file)
+
+
+def get_songs(path: str) -> list[LibrarySong]:
+    songs = []
+    for song_path in iterate_songs(path):
+        folder = Path(song_path).parent
+        playlist = folder.parent
+        try:
+            songs.append(LibrarySong(folder, playlist.name, Song.read(song_path)))
+        except TypeError:
+            continue
+    return songs
+
+
+def list_library(path: str):
+    for element in get_songs(path):
+        print(element.playlist, element.song.title)
