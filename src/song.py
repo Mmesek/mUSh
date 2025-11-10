4eb03a757404d84483465d7366717861a6b2f3dc
Mmesek <13630781+Mmesek@users.noreply.github.com>
2025-11-10 02:16:53 +0100

    Add interface to access transcriber/pitcher
    Signed-off-by: Mmesek <13630781+Mmesek@users.noreply.github.com>

--- src/song.py
+++ src/song.py
@@ -1,9 +1,13 @@
 import msgspec
 from models import Note, Song as SongSchema
 import separator
+import transcriber
+import pitcher
 
 
 class Song(SongSchema):
+    _path: str = None
+
     @classmethod
     def parse(cls, text: str) -> "Song":
         """Reads .txt file and parses it's structure"""
@@ -33,7 +37,9 @@ class Song(SongSchema):
     def dump(self) -> str:
         """Dumps structure into a text form"""
         text = ""
-        for attribute in self.__annotations__:
+        for attribute in super().__annotations__:
+            if attribute.startswith("_"):
+                continue
             if attribute != "notes":
                 text += f"#{attribute.upper()}={getattr(self, attribute)}\n"
 
@@ -43,14 +49,33 @@ class Song(SongSchema):
         text += "E"
         return text
 
+    def get_path(self, file: str):
+        if self._path:
+            return self._path + "/" + file
+        return file
+
     def separate_vocals(self, file_path: str = None):
         """Using demucs, separates `vocals` and `instrumental` audio from `audio` or `mp3`"""
         if self.vocals and self.instrumental:
             return
-        if file_path:
-            audio = file_path + "/" + self.audio
-        else:
-            audio = self.audio
-        output_dir = separator.separate(audio)
-        self.vocals = separator.rename_stem(output_dir, "vocals.mp3")
-        self.instrumental = separator.rename_stem(output_dir, "no_vocals.mp3")
+
+        output_dir = separator.separate(self.get_path(self.audio))
+        self.vocals = separator.convert(output_dir, "vocals.mp3")
+        self.instrumental = separator.convert(output_dir, "no_vocals.mp3")
+
+    def transcribe_vocals(self, file_path: str = None):
+        """Using whisperx, transcribes `vocals`"""
+        if not self.vocals:
+            self.separate_vocals(file_path)
+
+        self._transcription = transcriber.transcribe(self.get_path(self.vocals))
+
+    def pitch_vocals(self, file_path: str = None):
+        """Using crepe, detects pitch of `vocals`"""
+        if not self.vocals:
+            self.separate_vocals(file_path)
+
+        self._pitch_result = pitcher.detect_pitch(self.get_path(self.vocals))
+
+    def build_notes(self):
+        pass
