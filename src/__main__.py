87ebf8cd5a0e996eabed11f27523b598d7b68a3c
Mmesek <13630781+Mmesek@users.noreply.github.com>
2025-11-13 15:21:04 +0100

    Add moving to library CLI option
    Signed-off-by: Mmesek <13630781+Mmesek@users.noreply.github.com>

--- src/__main__.py
+++ src/__main__.py
@@ -10,6 +10,11 @@ parser.add_argument(
     help="Path to cache directory where output will be stored. Default is `out`",
     default="out",
 )
+parser.add_argument(
+    "-library",
+    help="Path to Ultrastar library",
+    default="mUSh",
+)
 
 if __name__ == "__main__":
     args = parser.parse_args()
@@ -18,5 +23,7 @@ if __name__ == "__main__":
     logger.info("Song %s - %s initiated", s.title, s.artist)
     s.build_notes()
     logger.info("Notes built")
-    s.write(args.output)
+    destination = s.move(args.library)
+    logger.info("Moved to library")
+    s.write(destination)
     logger.info("Written result to %s", args.output)
