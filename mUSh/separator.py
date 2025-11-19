07918af0d6392c39493c36a4755d0b648e1c497b
Mmesek <13630781+Mmesek@users.noreply.github.com>
2025-11-19 17:36:01 +0100

    Change print calls to logger calls
    Signed-off-by: Mmesek <13630781+Mmesek@users.noreply.github.com>

--- mUSh/separator.py
+++ mUSh/separator.py
@@ -3,6 +3,7 @@ from pathlib import Path
 
 import demucs.separate
 import ffmpeg
+from mUSh.cli import logger
 
 REUSE_OK = True
 
@@ -13,7 +14,7 @@ def separate(path: str, model: str = "htdemucs_ft", output: str = "out") -> Path
         if os.path.exists(result / "no_vocals.mp3") and os.path.exists(
             result / "vocals.mp3"
         ):
-            print("Stems already exists, skipping")
+            logger.debug("Stems already exists, skipping")
             return result
     demucs.separate.main(
         ["--mp3", "--two-stems", "vocals", "-n", model, "--out", output, path]
@@ -32,11 +33,11 @@ def convert(
     result = f"{path.name} [{stem_type}].{extension}"
     if os.path.exists(path / result):
         if REUSE_OK:
-            print("Reusing previous result")
+            logger.debug("Reusing previous result")
             return result
-        print("Deleting old result file")
+        logger.debug("Deleting old result file")
         os.remove(path / result)
     worker = ffmpeg.FFmpeg().input(path / stem).output(path / result)
-    print("Calling FFmpeg with:", worker.arguments)
+    logger.debug("Calling FFmpeg with: %s", worker.arguments)
     worker.execute()
     return result
